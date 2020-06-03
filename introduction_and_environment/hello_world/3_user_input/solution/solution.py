1ec11fe25947244721c9a54fcaa5434ef5d565ab
sandipsandal <64512191+sandipsandal@users.noreply.github.com>
2020-06-03 20:25:54 +0530

    pytest

--- introduction_and_environment/hello_world/3_user_input/solution/solution.py
+++ introduction_and_environment/hello_world/3_user_input/solution/solution.py
@@ -1,1 +1,5 @@
-# Code your solution here
+FIRST_NUM = float(input("Enter first no : "))
+SECOND_NUM = float(input("Enter second no : "))
+THIRD_NUM = float(input("Enter third no : "))
+average = (FIRST_NUM+SECOND_NUM+THIRD_NUM)/3
+print(average)
